48a4175702c99e2ae13cdbbfe82f7bb0914b5493
Alexander Heinrich <alxhnr@nudelpost.de>
2014-12-13 14:20:47 +0100

    Initial commit

---
+++ exec-path.scm
@@ -0,0 +1,22 @@
+(import dlaunch-plugin-api)
+(use posix data-structures srfi-1)
+
+(let ()
+  (register-source
+    "exec-path"
+    (lambda ()
+      (sort
+        (fold
+          (lambda (path lst)
+            (fold
+              cons lst
+              (condition-case
+                (directory path)
+                ((exn file) '()))))
+          '() (string-split (get-environment-variable "PATH") ":"))
+        string<?)))
+
+  (register-handler
+    (lambda (selected-string source-name)
+      (if (string=? source-name "exec-path")
+        (process-run selected-string)))))
